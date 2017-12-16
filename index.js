1b97d3946ccf612ebf5f022cabe7c5eec8627768
abugi <abugiboi@gmail.com>
2017-12-16 20:44:38 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const http = require('http');
+http.Server(app);
+
+app.get('/', function(req, res){
+     res.send('<h1>Hello World</h1>');
+});
+
+http.listen(3000, function(){
+     console.log('We are live on port 3K');
+});
