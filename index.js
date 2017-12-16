82e867b52e9e258751759d1858b632fa3946ac54
abugi <abugiboi@gmail.com>
2017-12-16 21:19:09 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
      res.sendFile(__dirname + '/index.html');
+});
+
+io.on('conneciton', function (socket) {
+     console.log('someone connected');
 });
 
 http.listen(3000, function () {
