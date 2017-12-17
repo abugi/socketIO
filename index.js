bb91030a10dd3286d10d85195e5529d344581a63
abugi <abugiboi@gmail.com>
2017-12-17 22:11:32 +0100

    initial commit

--- index.js
+++ index.js
@@ -15,3 +15,14 @@
 // http.listen(3000, function () {
 //      console.log('listening on *:3000');
 // });
+var http = require("http");
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<html>");
+    response.write("<head><title>Node.js</title></head>");
+    response.write("<body>Hello Web</body>");
+    response.write("</html>");
+    response.end();
+});
+
+http.listen(9999);
