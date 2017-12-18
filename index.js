14b995459939ec484364c24ca62749b38500038d
abugi <abugiboi@gmail.com>
2017-12-18 12:58:54 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-// const app = require('express')();
-// const http = require('http').Server(app);
-// const io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// app.get('/', function (req, res) {
-//      res.sendFile(__dirname + '/index.html');
-// });
+app.get('/', function (req, res) {
+     res.sendFile(__dirname + '/index.html');
+});
 
-// io.on('connection', function (socket) {
-//      socket.on('chat message', function (msg) {
-//           io.emit('chat message', msg);
-//      });
-// });
+io.on('connection', function (socket) {
+     socket.on('chat message', function (msg) {
+          io.emit('chat message', msg);
+     });
+});
 
-// http.listen(3000, function () {
-//      console.log('listening on *:3000');
-// });
-
-var http = require("http");
-http.createServer(function (request, response) {
-    // response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("<html>");
-    response.write("<head><title>Intro to Node.js</title></head>");
-    response.write("<body>Hello Web</body>");
-    response.write("</html>");
-    response.end();
-}).listen(9999);
+http.listen(3000, function () {
+     console.log('listening on *:3000');
+});
