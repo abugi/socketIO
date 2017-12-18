eaa93a23de3b6ce02bb0eec2bb3f250257064c21
abugi <abugiboi@gmail.com>
2017-12-18 15:34:11 +0100

    initial commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+    console.log(socket);
      socket.on('chat message', function (msg) {
           io.emit('chat message', msg);
      });
